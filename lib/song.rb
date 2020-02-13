95919b03d2513be2d166e97a2f5e0f236357657f
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 07:40:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,5 +18,7 @@ class Song
   def self.new_by_filename(filename)
     names = filename.split(" - ")
     song = self.new(names[1])
-
+    song.artist =Artist.find_or_create_by_name(names[0])
+    song
+  end
 end
